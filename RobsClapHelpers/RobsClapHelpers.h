ad66f5342267324e3bb170568609f8a69089c445
Robin Schmidt <robin@rs-met.com>
2024-04-02 21:30:42 +0200

    Update RobsClapHelpers.h

--- RobsClapHelpers/RobsClapHelpers.h
+++ RobsClapHelpers/RobsClapHelpers.h
@@ -1,5 +1,11 @@
 #pramga once
 
+/** Main include file for RobsClapHelpers. To use these helpers, you need to include this file 
+somewhere in your code and add the RobsClapHelpers.cpp file to your project. The latter is a unity
+build file meaning that it includes all the .cpp files from RobsClapHelpers and compiles them in
+one compilation unit. If you want want to add the separate .cpp files to your project for browsing
+the code, you need to exclude them from the build. */
+
 #include "Utilities.h"
 #include "ClapPlugin.h"
 #include "ClapPluginClasses.h"
